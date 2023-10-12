e44d882ff5f6ec68403ab340093f3ed7a835a2fd
Md. Sabik Alam Rahat <sabikrahat72428@gmail.com>
2023-10-12 14:39:11 +0600

    tpken verify added

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const checkAuth = require("../middleware/check-auth");
 
 const Product = require("../models/product");
 const Order = require("../models/order");
 
-router.get("/", (req, res, next) => {
+router.get("/", checkAuth, (req, res, next) => {
     Order.find()
         .select("product quantity _id")
         .populate("product", "name price _id")
@@ -36,7 +37,7 @@ router.get("/", (req, res, next) => {
         });
 });
 
-router.post("/", (req, res, next) => {
+router.post("/", checkAuth, (req, res, next) => {
     Product.findById(req.body.productId)
         .then(product => {
             if (!product) {
@@ -73,7 +74,7 @@ router.post("/", (req, res, next) => {
         });
 });
 
-router.get("/:orderId", (req, res, next) => {
+router.get("/:orderId", checkAuth, (req, res, next) => {
     Order.findById(req.params.orderId)
         .select("product quantity _id")
         .populate("product", "name price _id")
@@ -95,7 +96,7 @@ router.get("/:orderId", (req, res, next) => {
         });
 });
 
-router.delete("/:orderId", (req, res, next) => {
+router.delete("/:orderId", checkAuth, (req, res, next) => {
     Order.deleteOne({ _id: req.params.orderId })
         .exec()
         .then(result => {
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const checkAuth = require("../middleware/check-auth");
 
 const multer = require("multer");
 const storage = multer.diskStorage({
@@ -54,7 +55,7 @@ router.get("/", (req, res, next) => {
         });
 });
 
-router.post("/", upload.single('productImage'), (req, res, next) => {
+router.post("/", checkAuth, upload.single('productImage'), (req, res, next) => {
     console.log(req.file);
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
@@ -109,7 +110,7 @@ router.get("/:productId", (req, res, next) => {
         });
 });
 
-router.patch("/:productId", (req, res, next) => {
+router.patch("/:productId", checkAuth, (req, res, next) => {
     const id = req.params.productId;
     const updateOps = {};
     for (const ops of req.body) {
@@ -146,7 +147,7 @@ router.patch("/:productId", (req, res, next) => {
         });
 });
 
-router.delete("/:productId", (req, res, next) => {
+router.delete("/:productId", checkAuth, (req, res, next) => {
     const id = req.params.productId;
     Product.deleteOne({ _id: id })
         .exec()
